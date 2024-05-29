5d9af01b900760dee3e609e040bbbc0e8148f73f
CWDamm <cwdamm@gmail.com>
2024-05-29 16:51:47 +0100

    add endpoint for deleting a comment with a given id

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -394,6 +394,32 @@ describe('PATCH /api/articles/:article_id', () => {
 
 })
 
+describe("delete a given comment by comment id", () => {
+    test("DELETE /api/comments/:comment_id", () => {
+        return request(app)
+        .delete("/api/comments/1")
+        .expect(204)
+    })
+
+    test('DELETE:404 responds with an appropriate status and error message when given a valid but non-existent id', () => {
+        return request(app)
+        .delete("/api/comments/999999")
+        .expect(404)
+        .then(response => {
+            expect(response.body.msg).toBe("Comment not found")
+        })
+    })
+
+    test('DELETE:400 responds with an appropriate status and error message when given an invalid id', () => {
+        return request(app)
+        .delete("/api/comments/not-a-comment")
+          .expect(400)
+          .then(response => {
+            expect(response.body.msg).toBe('Bad request');
+          });
+      });
+
+})
 
 
 
